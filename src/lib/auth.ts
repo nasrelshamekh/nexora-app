5b3f261ffef3d266695061ed77b81b12ba654d9b
nasrelshamekh <nasrelshamekh@gmail.com>
2026-01-15 19:00:05 +0200

    token update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,8 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-    const encryptedToken = (await cookies()).get("next-auth.session-token")?.value
+    const myCookies = await cookies()
+    const encryptedToken = myCookies.get("next-auth.session-token")?.value || myCookies.get("__Secure-next-auth.session-token")?.value
     const token = await decode({ token: encryptedToken, secret: process.env.AUTH_SECRET! })
     return token?.token as string
 }
